4ceece9c5ed55667611ed1cde9eb7551ccca993d
Andrew <leinga@rpi.edu>
2013-04-27 03:35:25 -0400

    CAUSE I CAN
    Tidied up the GUI for the starting screen.

--- Colosseum_Arcana/Assets/Scripts/StartScreen.cs
+++ Colosseum_Arcana/Assets/Scripts/StartScreen.cs
@@ -35,27 +35,27 @@ public class StartScreen : MonoBehaviour
 	
 	void OnGUI()
 	{
-		GUI.Label(new Rect(Screen.width*.5f - 175,10,450, 100), "\n\t\t\t\t\t\t\t\tElemental Fury", GUIstuff.textArea);
+		GUI.Box(new Rect(Screen.width*.5f - 225,10,450, 100), "\n\t\t\t\t\t\t\t\tElemental Fury", GUIstuff.textArea);
 		if(!PlayGame && !EnterEditor && !QuitGame)
 		{
-			if(GUI.Button(new Rect(Screen.width *.5f - 45,110,100,30), "Play Game", GUIstuff.button))
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 150,100,30), "Play Game", GUIstuff.button))
 			{
 				PlayGame = true;
 			}
-			if(GUI.Button(new Rect(Screen.width *.5f - 45,210,100,30), "Map Editor", GUIstuff.button))
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 100,100,30), "Map Editor", GUIstuff.button))
 			{
 				EnterEditor = true;
 			}
-			if(GUI.Button(new Rect(Screen.width *.5f - 45,310,100,30), "Quit", GUIstuff.button))
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 50,100,30), "Quit", GUIstuff.button))
 			{
 				QuitGame = true;
 			}
 		}
 		if(PlayGame)
 		{
-			GUI.Label(new Rect(Screen.width *.5f - 100,115,300,30), "\t\tLoad the default map or load a saved map", GUIstuff.label);
-			LevelName = GUI.TextField(new Rect(Screen.width *.5f - 45,200,100,30), LevelName, GUIstuff.textField);
-			if(GUI.Button(new Rect(Screen.width *.5f - 50,250,100,30), "Play", GUIstuff.button))
+			GUI.Box(new Rect(Screen.width *.5f - 150,Screen.height *.5f - 150,300,30), "\t\tLoad the default map or load a saved map", GUIstuff.box);
+			LevelName = GUI.TextField(new Rect(Screen.width *.5f - 100,Screen.height *.5f - 100,200,30), LevelName, GUIstuff.textField);
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 50,100,30), "Play", GUIstuff.button))
 			{
 				if(File.Exists("C:/ElementalFury/Maps/" + LevelName + ".xml"))
 				{
@@ -71,9 +71,9 @@ public class StartScreen : MonoBehaviour
 			}
 			if(LoadError)
 				{
-					GUI.Label(new Rect(Screen.width *.5f - 150,150,300,30), "Map '" + ErrorLevel + "' does not exist.", GUIstuff.label);
+					GUI.Box(new Rect(Screen.width *.5f - 250,Screen.height - 50,500,30), "Map '" + ErrorLevel + "' does not exist.", GUIstuff.box);
 				}
-			if(GUI.Button(new Rect(Screen.width *.5f - 50,300,100,30), "Return", GUIstuff.button))
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f,100,30), "Return", GUIstuff.button))
 			{
 				PlayGame = false;
 				LoadError = false;
@@ -82,27 +82,27 @@ public class StartScreen : MonoBehaviour
 		}
 		if(EnterEditor)
 		{
-			GUI.Label(new Rect(100,100,300,30), "Are you sure you want to start the editor?", GUIstuff.label);
-			if(GUI.Button(new Rect(Screen.width *.5f - 50,200,100,30), "Start Editor", GUIstuff.button))
+			GUI.Box(new Rect(Screen.width *.5f - 150,Screen.height *.5f - 150,300,30), "Are you sure you want to start the editor?", GUIstuff.box);
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 100,100,30), "Start Editor", GUIstuff.button))
 			{
 				//Load Map Editor
 				Application.LoadLevel("Builder");
 			}
 				
-			if(GUI.Button(new Rect(Screen.width *.5f - 50,300,100,30), "Return", GUIstuff.button))
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 50,100,30), "Return", GUIstuff.button))
 			{
 				EnterEditor = false;
 			}
 		}
 		if(QuitGame)
 		{
-			GUI.Label(new Rect(Screen.width *.5f - 150,100,300,30), "Are you sure you want to quit?", GUIstuff.label);
-			if(GUI.Button(new Rect(Screen.width *.5f - 50,200,100,30), "Yes", GUIstuff.button))
+			GUI.Box(new Rect(Screen.width *.5f - 150,Screen.height *.5f - 150,300,30), "Are you sure you want to quit?", GUIstuff.box);
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 100,100,30), "Yes", GUIstuff.button))
 			{
 				Application.Quit();
 			}
 				
-			if(GUI.Button(new Rect(Screen.width *.5f - 50,300,100,30), "No", GUIstuff.button))
+			if(GUI.Button(new Rect(Screen.width *.5f - 50,Screen.height *.5f - 50,100,30), "No", GUIstuff.button))
 			{
 				QuitGame = false;
 			}
